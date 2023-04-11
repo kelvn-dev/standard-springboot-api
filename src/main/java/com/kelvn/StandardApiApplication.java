2fa76112e02259cfab28f86069886e362c2d92e2
kelvn <kelvn.developer@gmail.com>
2023-04-12 00:05:02 +0700

    First commit to init project

---
+++ src/main/java/com/kelvn/StandardApiApplication.java
@@ -0,0 +1,13 @@
+package com.kelvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandardApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StandardApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kelvn/StandardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StandardApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
