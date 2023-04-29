ff7fa9146e43843b93baa2451f82009b6531684b
kelvn <kelvn.developer@gmail.com>
2023-04-29 10:25:36 +0700

    Seperating auditing configuration

---
+++ src/main/java/com/kelvn/config/AuditingConfig.java
@@ -0,0 +1,18 @@
+package com.kelvn.config;
+
+import com.kelvn.security.SecurityAuditorAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef="auditorAware")
+public class AuditingConfig {
+
+  @Bean
+  public AuditorAware<String> auditorAware() {
+    return new SecurityAuditorAware();
+  }
+
+}
