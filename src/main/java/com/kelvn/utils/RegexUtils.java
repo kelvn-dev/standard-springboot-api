63943692e2e5cc79cf24a526246b6299e47fdc19
khanh <khanh.vu@mati.com.vn>
2023-07-15 18:58:54 +0700

    Update javadocs

--- src/main/java/com/kelvn/utils/RegexUtils.java
+++ src/main/java/com/kelvn/utils/RegexUtils.java
@@ -2,10 +2,12 @@ package com.kelvn.utils;
 
 public class RegexUtils {
   /**
-   * ^ asserts the start of the string. [\\w\\s\\-()]+ matches one or more word characters
-   * (alphanumeric and underscore), whitespace characters, hyphens, parentheses, or underscores. \\.
-   * matches a literal dot character. [a-zA-Z]{2,4} matches two to four alphabetic characters. You
-   * can adjust the range as needed. $ asserts the end of the string.
+   * @note ^ asserts the start of the string.
+   * @note [\\w\\s\\-()]+ matches one or more word characters (alphanumeric and underscore),
+   *     whitespace characters, hyphens, parentheses, or underscores.
+   * @note \\.matches a literal dot character.
+   * @note [a-zA-Z]{2,4} matches two to four alphabetic characters.
+   * @note $ asserts the end of the string.
    */
   public static final String simpleFilename = "^[\\w\\s\\-()]+\\.[a-zA-Z]{2,4}$";
 }
